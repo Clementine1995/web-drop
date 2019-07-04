ad9d226c9a7116229f3b2eb236a804ca5eff892a
Clementine1995 <18669859508@163.com>
2019-07-04 20:09:56 +0800

    提交

--- front-end/webGL/learn/webgl-helper.js
+++ front-end/webGL/learn/webgl-helper.js
@@ -135,3 +135,19 @@ function createBuffer(gl, attribute, vertexAttribPointer) {
   );
   return buffer;
 }
+
+function loadTexture(gl, src, attribute, callback) {
+  let img = new Image();
+  img.crossOrigin = 'anonymous';
+  img.onload = function() {
+    gl.activeTexture(gl.TEXTURE0);
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+    gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.uniform1i(attribute, 0);
+    callback && callback();
+  };
+  img.src = src;
+}
